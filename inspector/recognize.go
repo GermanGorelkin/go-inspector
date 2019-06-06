c8cdd81ca59079afd4d9d7c5052db6ef39f7e300
German Gorelkin <german.gorelkin@gmail.com>
2019-06-06 22:05:47 +0300

    add RecognitionError

--- inspector/recognize.go
+++ inspector/recognize.go
@@ -18,7 +18,6 @@ type RecognizeRequest struct {
 	Webhook     *string    `json:"webhook,omitempty"`
 	CountryCode *string    `json:"country_code"`
 }
-
 type RecognizeResponse struct {
 	ID      int            `json:"id"`
 	Images  []int          `json:"images"`
@@ -34,6 +33,31 @@ func (srv *RecognizeService) Recognize(rr *RecognizeRequest) (*RecognizeResponse
 	}
 
 	var rec RecognizeResponse
+	_, err = srv.client.do(req, &rec)
+	if err != nil {
+		return nil, err
+	}
+
+	return &rec, nil
+}
+
+type RecognitionErrorRequest struct {
+	Images  []int  `json:"images"`
+	SkuId   int    `json:"sku_gid"`
+	Scene   string `json:"scene"`
+	Message string `json:"message"`
+}
+type RecognitionErrorResponse struct {
+	RecognitionErrorID int `json:"recognition_error_id"`
+}
+
+func (srv *RecognizeService) RecognitionError(rr *RecognitionErrorRequest) (*RecognitionErrorResponse, error) {
+	req, err := srv.client.newRequest("POST", "recognition_error/", rr)
+	if err != nil {
+		return nil, err
+	}
+
+	var rec RecognitionErrorResponse
 	_, err = srv.client.do(req, &rec)
 	if err != nil {
 		return nil, err
